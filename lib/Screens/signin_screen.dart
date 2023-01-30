a3196bda0664ac2b6a9401f4d24e3a8f527facc8
Aditya <adityavishal903@gmail.com>
2023-01-30 22:52:19 +0530

    2

--- lib/Screens/signin_screen.dart
+++ lib/Screens/signin_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_application_1/Screens/util/color_utils.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -12,6 +13,18 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+        hexStringToColor("CB2893"),
+        hexStringToColor("9546C4"),
+        hexStringToColor("5E61F4"),
+      ], begin: Alignment.topCenter, end: Alignment.bottomCenter
+    ),
+  ),
+),
+);
+
   }
 }
